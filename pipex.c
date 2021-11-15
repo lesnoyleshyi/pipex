e737e3b3424c4bb0384e68e87d7df97bdc41f16c
stycho <stycho@student.21-school.ru>
2021-11-15 17:44:24 +0300

    arguments checking should works fine (ft_check_args) but strnstr in ft_get_paths must be rewritten to strstr (without N parameter)

--- pipex.c
+++ pipex.c
@@ -11,31 +11,28 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "libft/libft.h"
 
 int	main(int argc, char *argv[])
 {
 	char *error_msg;
 
-	if (ft_check_args(argc, char *argv[]))
-		return (1);
+	ft_check_args(argc, char *argv[]));
+
 
 }
 
-int	ft_check_args(int argc, char **argv)
+void	ft_check_args(int argc, char **argv)
 {
 	int	i;
 
 	if (argc != 5)
-	{
-		write(2, "Wrong arguments count\n", 23);
-		exit(1);
-	}
+		ft_pmessage_and_exit("Wrong arguments count\n", 2);
 	if (access(argv[1], R_OK) == -1)
 		ft_perror_and_exit("Program exit with message");
 	i = 2;
 	while (i < argc)
 		ft_check_rights(argv[i++]);
-	return (0);
 }
 
 void	ft_check_rights(char *arg, char *envp[])
@@ -44,32 +41,66 @@ void	ft_check_rights(char *arg, char *envp[])
 	char	**paths;
 	char	*path_to_check;
 
-	i = 0;
-	paths = NULL;
-	while (envp[i])
+	path_to_check = ft_strjoin("/bin/", arg);
+	if (access(path_to_check, X_OK) == 0)
 	{
-		if (ft_strstr(envp[i], "PATH="))
-		{
-			paths = ft_split(envp[i++], ':');		// +5 to avoid PATH=
-			break ;
-		}
+		free(path_to_check);
+		return ;
 	}
+	paths = ft_get_paths(envp);
 	if (!paths)
-	{
-		write(2, "command not found, exit\n", 24);
-		exit(1);
-	}
+		ft_pmessage_and_exit("command not found, exit");
 	i = 0;
-	while (str[i])
+	while (paths[i])
 	{
-		path_to_check = ft_strjoin(str[i++], arg);
-
+		path_to_check = ft_strjoin(paths[i++], arg);
+		if (access(path_to_check, X_OK) == 0)
+		{
+			free(path_to_check);
+			ft_free_array(paths);
+			return	;
+		}
 		free(path_to_check);
 	}
+	free(paths);
+	ft_perror_and_exit("Program exit with message");
 }
 
 void	ft_perror_and_exit(const char *message)
 {
 	perror(message);
-	exit(1);
+	exit(-1);
+}
+
+void	ft_pmessage_and_exit(const char *message)
+{
+	ft_putstr_fd(message, 2);
+	exit(-1);
+}
+
+char	**ft_get_paths(char *envp[])
+{
+	int 	i;
+	char	**paths;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", 1000))
+		{
+			paths = ft_split((envp[i++]) + 5, ':');
+			break ;
+		}
+	}
+	return (paths);
+}
+
+void	ft_free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free(array);
 }
--- pipex.h
+++ pipex.h
@@ -16,6 +16,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 void	ft_check_rights(char *arg);
+void	ft_free_array(char **array);
 void	ft_perror_and_exit(const char *message);
+void	ft_pmessage_and_exit(const char *message);
 int		ft_check_args(int argc, char **argv);
+char	**ft_get_paths(char *envp[]);
 #endif
